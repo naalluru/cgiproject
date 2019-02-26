37caab69b80e7f1f0c56dd21d63f88fd9907f64e
naalluru <alluri.nagaraju@gmail.com>
2019-02-26 16:15:39 +0530

    removed new changes
    added

--- CgiDevelopment.java
+++ CgiDevelopment.java
@@ -3,6 +3,6 @@ package com.cgi.dev;
 public class CgiDevelopment {
 public static void main(String[] args) {
 	System.out.println("This is sample maven project");
-	System.out.println("This is sample maven project added in jenkin");
+	
 }
 }
